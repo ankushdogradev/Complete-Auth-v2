5d7df23b9e531e2956066d6f52bd5dba31200208
ankushdogradev <ankushdogradev@gmail.com>
2021-08-01 08:57:56 +0530

    Auto delete user data after 7 days if not verified

--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ require("dotenv").config({
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const schedule = require("node-schedule");
 const connectDB = require("./config/db");
 const errorHandler = require("./error/errorHandler");
+const User = require("./models/userModels");
 const authRoutes = require("./routes/authRoutes");
 
 connectDB();
@@ -18,6 +20,22 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api", authRoutes);
+
+// >>>>>>> ~ Delete User Data if not verified ~ <<<<<<<
+const job = schedule.scheduleJob("0 1 * * *", function () {
+  return deleteOldUsers();
+});
+
+const deleteOldUsers = () => {
+  const current = new Date();
+  // subtracting 7 days
+  current.setDate(current.getDate() - 7);
+  User.deleteMany({ isVerify: false, createdAt: { $lte: current } }, (err) => {
+    if (err) return console.log("Error while erasing users " + err);
+    console.log("successfully erased data");
+  });
+};
+// >>>>>>> ~ Delete User Data if not verified ~ <<<<<<<
 
 app.use(errorHandler);
 
