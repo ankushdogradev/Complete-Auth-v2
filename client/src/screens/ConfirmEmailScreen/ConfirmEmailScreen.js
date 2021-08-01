50acbca2a7dec95143ee8aa3b6419ea26374e0c2
ankushdogradev <ankushdogradev@gmail.com>
2021-08-01 09:33:11 +0530

    Fixed useEffect() async warning

--- client/src/screens/ConfirmEmailScreen/ConfirmEmailScreen.js
+++ client/src/screens/ConfirmEmailScreen/ConfirmEmailScreen.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { Redirect } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -8,15 +7,31 @@ import "./ConfirmEmailScreen.scss";
 const ConfirmEmailScreen = ({ match, history }) => {
   const activationToken = match.params.activationToken;
 
-  useEffect(async () => {
-    try {
-      const { data } = await axios.put(
-        `http://127.0.0.1:5000/api/account-activation/${activationToken}`
-      );
-      if (data.user.isVerify) {
-        console.log("Redirecting... to /login-register");
-        // return <Redirect to="/login-register" />;
-        toast.success(`Email Verified, you will be redirected shortly`, {
+  useEffect(() => {
+    const activate = async () => {
+      try {
+        const { data } = await axios.put(
+          `http://127.0.0.1:5000/api/account-activation/${activationToken}`
+        );
+        if (data.user.isVerify) {
+          console.log("Redirecting... to /login-register");
+          // return <Redirect to="/login-register" />;
+          toast.success(`Email Verified, you will be redirected shortly`, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+          window.setTimeout(() => {
+            return history.push("/login-register");
+          }, 3000);
+        }
+      } catch (error) {
+        console.log("Error >>> ", error.response.data.error);
+        toast.error(`${error.response.data.error}`, {
           position: "top-center",
           autoClose: 5000,
           hideProgressBar: false,
@@ -25,23 +40,11 @@ const ConfirmEmailScreen = ({ match, history }) => {
           draggable: true,
           progress: undefined,
         });
-        window.setTimeout(() => {
-          return history.push("/login-register");
-        }, 3000);
       }
-    } catch (error) {
-      console.log("Error >>> ", error.response.data.error);
-      toast.error(`${error.response.data.error}`, {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    }
-  }, []);
+    };
+
+    activate();
+  }, [activationToken, history]);
   return (
     <>
       <div className="confirm-bg">
